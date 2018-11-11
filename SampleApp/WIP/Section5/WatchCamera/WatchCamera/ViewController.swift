3f53008a7bf582318fae23e26a38e58487128d88
iOS12カリキュラム <>
2018-11-11 14:49:07 +0900

    WatchCameraのプロジェクト作成〜設定まで

---
+++ SampleApp/WIP/Section5/WatchCamera/WatchCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchCamera
+//
+//  Created by iOS12カリキュラム on 2018/11/11.
+//  Copyright © 2018 UDONKONET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
